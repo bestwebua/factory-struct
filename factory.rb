e2cb896ddbac4a2107094e355688525977c3e5b8
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-05-07 17:58:03 +0300

    Complete #initialize for dynamically subclass

--- factory.rb
+++ factory.rb
@@ -8,20 +8,26 @@ class Factory
   include Enumerable
 
   def self.new(*args, &block)
-    constant, params = args[0], args[1..-1]
+    constant, class_args = args[0], args[1..-1]
 
     case
       when args.empty?
         raise ArgumentError, 'wrong number of arguments (given 0, expected 1+)'
       when constant.nil? && !constant.is_a?(String) && !constant.is_a_const?
         raise NameError, "identifier #{constant} needs to be constant"
-      when !params.empty? && params.all? { |item| !item.is_a?(Symbol) }
-        raise TypeError, "#{params[0]} is not a symbol"
+      when !class_args.empty? && class_args.all? { |item| !item.is_a?(Symbol) }
+        raise TypeError, "#{class_args[0]} is not a symbol"
     end
 
-      temp_class = Class.new Factory do
-        
+    temp_class = Class.new do # need to add inheritance from Factory class, exception method .new
+      attr_accessor *class_args
+
+      define_method :initialize do |*args|
+        subclass_args = Array.new(class_args.size).map.with_index { |item, index| args[index] }
+        raise ArgumentError, 'factory size differs' if args.size > class_args.size
+        class_args.zip(subclass_args).each { |accessor, value| public_send("#{accessor}=", value) }
       end
+    end
 
     constant.nil? ? temp_class : const_set(constant, temp_class)
   end
