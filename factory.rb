d88a62cc11614658b807c54a4262dc9f1198f7e7
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-05-18 14:28:35 +0300

    Update public_method visibility, refactor #values_at

--- factory.rb
+++ factory.rb
@@ -20,8 +20,7 @@ class Factory
         raise TypeError, "#{class_args[0]} is not a symbol nor a string" if class_args.all? { |i| !i.is_a?(Symbol) }
     end
 
-    subclass = Class.new Factory do
-
+    subclass = Class.new self do
       attr_accessor *class_args
 
       define_singleton_method(:new) do |*args|
@@ -35,96 +34,90 @@ class Factory
         raise ArgumentError, 'factory size differs' if args.size > class_args.size
         class_args.zip(subclass_args).each { |accessor, value| public_send("#{accessor}=", value) }
       end
-
-      define_method(:==) do |other|
-        self.class == other.class && self.values == other.values
-      end
-
-      define_method(:[]) do |arg|
-        if arg.is_a?(Integer)
-          index_error = "offset #{arg} too large for factory(size:#{self.size})"
-          raise IndexError, index_error unless arg.between?(0, attributes.size-1)
-          send(attributes[arg])
-        else
-          name_error = "no member '#{arg}' in factory"
-          raise NameError, name_error unless attributes.include?(arg)
-          send(arg)
-        end
-      end
-
-      define_method(:[]=) do |arg, value|
-        self[arg]
-        attribute = if arg.is_a?(Integer)
-          attributes[arg].to_s.insert(0, '@').to_sym
-        else
-          arg.to_s.insert(0, '@').to_sym
-        end
-        instance_variable_set(attribute, value)
-      end
-
-      define_method(:dig) do |*args|
-        to_h.dig(*args)
-      end
-
-      define_method(:each) do |&block|
-        return enum_for(:each) unless block
-        attributes.each { |attribute| block.call(send(attribute)) }
-        self
-      end
-
-      define_method(:each_pair) do |&block|
-        return enum_for(:each) unless block
-        to_h.each_pair(&block)
-      end
-
-      define_method(:eql?) do |other|
-        self.class == other.class && (self.values).eql?(other.values)
-      end
-
-      define_method(:length) do
-        attributes.size
-      end
-
-      define_method(:members) do
-        attributes
-      end
-
-      define_method(:select) do |&block|
-        return enum_for(:select) unless block
-        values.select(&block)
-      end
-
-      define_method(:to_h) do
-        attributes.zip(values).to_h
-      end
-
-      define_method(:values) do
-        attributes.map { |attribute| send(attribute) }
-      end
-
-      define_method(:values_at) do |from, to|
-        result = values.values_at(from, to)
-        error = case
-            when result.first.nil? then "offset #{from} too small"
-            when result.last.nil? then "offset #{to} too large"
-          end
-        raise IndexError, error + " for factory(size:#{size})" if result.any?(&:nil?)
-        result
-      end
-
-      define_method(:attributes) do
-        instance_variables.map { |instance_var| instance_var.to_s.delete('@').to_sym }
-      end
-
-      alias :size :length
-      alias :to_a :values
-      alias :to_s :inspect
-      
-      private :attributes
-
     end
 
     constant.nil? ? subclass : const_set(constant, subclass)
-
   end
+
+  def == (other)
+    self.class == other.class && self.values == other.values
+  end
+
+  def [] (arg)
+    if arg.is_a?(Integer)
+      index_error = "offset #{arg} too large for factory(size:#{self.size})"
+      raise IndexError, index_error unless arg.between?(0, members.size-1)
+      send(members[arg])
+    else
+      name_error = "no member '#{arg}' in factory"
+      raise NameError, name_error unless members.include?(arg)
+      send(arg)
+    end
+  end
+
+  def []= (arg, value)
+    self[arg]
+      attribute = if arg.is_a?(Integer)
+        :"#{members[arg].to_s.insert(0, '@')}"
+      else
+        :"#{arg.to_s.insert(0, '@')}"
+      end
+    instance_variable_set(attribute, value)
+  end
+
+  def dig (*args)
+    to_h.dig(*args)
+  end
+
+  def each (&block)
+    return enum_for(:each) unless block
+    members.each { |attribute| block.call(send(attribute)) }
+    self
+  end
+
+  def each_pair (&block)
+    return enum_for(:each) unless block
+    to_h.each_pair(&block)
+  end
+
+  def eql? (other)
+    self.class == other.class && (self.values).eql?(other.values)
+  end
+
+  def length
+    members.size
+  end
+
+  def members
+    instance_variables.map { |instance_var| :"#{instance_var.to_s.delete('@')}" }
+  end
+
+  def select (&block)
+    return enum_for(:select) unless block
+    values.select(&block)
+  end
+
+  def to_h
+    members.zip(values).to_h
+  end
+
+  def values
+    members.map { |attribute| send(attribute) }
+  end
+
+  def values_at (*args)
+    args.select { |arg| arg.is_a?(Integer) }.each do |arg|
+      error = case
+        when arg < -values.size then "offset #{arg} too small"
+        when arg > values.size-1 then "offset #{arg} too large"
+      end
+      raise IndexError, error + " for factory(size:#{size})" if error
+    end
+    values.values_at(*args)
+  end
+
+  alias :size :length
+  alias :to_a :values
+  alias :to_s :inspect
+
 end
